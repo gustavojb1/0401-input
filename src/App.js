2a091e57528e081c59aa9e44cffffbd54f6d6bea
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2022-11-16 17:46:15 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [form, setForm] = React.useState({
+    nome:'',
+    email:''
+  })
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log(event)
+  }
+  function handleChange({target}){
+    const {id, value} = target
+    // O Couchete do id é para o react entender que é uma variável id, criada na linha acima
+    setForm({...form, [id]:value})
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome:</label>
+      <input
+        id="nome"
+        type="text"
+        name="nome"
+        value={form.nome}
+        onChange={handleChange}
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Enviar</button>
+      <p>Meu Nome é: {form.nome}</p>
+      <p>Meu Email é: {form.email}</p>
+    </form>
   );
 }
 
